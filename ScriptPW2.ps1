6abaa4cf6eef39e444e93e739a3d66441b0c6a8b
AntonioNeto504 <antoniocustorioneto@gmail.com>
2024-03-30 22:04:18 -0300

    AtividadePowerShell2_Devops

---
+++ ScriptPW2.ps1
@@ -0,0 +1,9 @@
+$nomeDoServico = "NomeDoServicoAqui"
+$service = Get-Service -Name $nomeDoServico
+
+if ($service.Status -ne "Running") {
+    Start-Service -Name $nomeDoServico
+    Write-Host "O serviço $nomeDoServico foi iniciado com sucesso."
+} else {
+    Write-Host "O serviço $nomeDoServico já está em execução."
+}
